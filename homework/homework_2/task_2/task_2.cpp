13a3129631a1ea53a9c7a4106642b9b5d64e7220
Sebastian Tleye <stleye@gmail.com>
2024-06-29 13:02:52 -0300

    Added task_2 for homework_2

--- homework/homework_2/task_2/task_2.cpp
+++ homework/homework_2/task_2/task_2.cpp
@@ -1,4 +1,3 @@
-#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -14,35 +13,29 @@ int main(int argc, const char *argv[]) {
   std::stringstream filename1(argv[1]);
   std::stringstream filename2(argv[2]);
 
-  int num1, num2 = 0;
-  std::string str1, str2 = "";
   std::string ext1, ext2;
+  int num1, num2;
 
-  filename1 >> str1 >> ext1;
-  filename2 >> str2 >> ext2;
   filename1 >> num1 >> ext1;
   filename2 >> num2 >> ext2;
 
-  std::cout << str1 << std::endl;
-  std::cout << str2 << std::endl;
-  std::cout << num1 << std::endl;
-  std::cout << num2 << std::endl;
+  if (filename1.fail() || filename2.fail()) {
+    std::cout << "Invalid filenames" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  //   if (num1 == "0" || num2 == "0") {
-  //     return EXIT_FAILURE;
-  //   }
-
-  //   if (ext1 == ext2) {
-  //     if (ext1 == ".txt") {
-  //       std::cout << "The mean is: " << (num1 + num2) / 2 << std::endl;
-  //     } else if (ext1 == ".png") {
-  //       std::cout << "The sum is: " << (num1 + num2) << std::endl;
-  //     }
-  //   } else if (ext1 == ".txt" && ext2 == ".png") {
-  //     std::cout << "The modulo division is: " << (num1 % num2) << std::endl;
-  //   } else {
-  //     std::cerr << "Types do not match expectations" << std::endl;
-  //   }
+  if (ext1 == ext2) {
+    if (ext1 == ".txt") {
+      std::cout << (num1 + num2) / 2 << std::endl;
+    } else if (ext1 == ".png") {
+      std::cout << (num1 + num2) << std::endl;
+    }
+  } else if (ext1 == ".txt" && ext2 == ".png") {
+    std::cout << (num1 % num2) << std::endl;
+  } else {
+    std::cerr << "Incorrect filename extension" << std::endl;
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
