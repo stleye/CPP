996a5d5648bc81bb2cab951cc3d093dd3b4cda4a
Sebastian Tleye <stleye@gmail.com>
2024-07-06 09:28:51 -0300

    More on homework4

--- homework/homework_4/src/main.cpp
+++ homework/homework_4/src/main.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <vector>
 
-using int_vector = ipb::named_vector<int>;
+// using int_vector = ipb::named_vector<int>;
 
 int main() {
-  int_vector named_vector{"name", {1, 2, 3, 4}};
-  std::cout << std::boolalpha << named_vector.empty() << std::endl;
-  std::cout << std::boolalpha << named_vector.name().empty() << std::endl;
+  // int_vector named_vector{"name", {1, 2, 3, 4}};
+  //  std::cout << std::boolalpha << named_vector.empty() << std::endl;
+  //  std::cout << std::boolalpha << named_vector.name().empty() << std::endl;
 
-  const int k = -100;
-  ipb::fill(named_vector, k);
+  // const int k = -100;
+  // ipb::fill(named_vector, k);
+  return 0;
 }
